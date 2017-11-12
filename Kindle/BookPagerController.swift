94481410ca1924e429845e91d9afb134948f341f
Darragh Blake <darragh.blake48@gmail.com>
2017-11-12 16:58:34 +0000

    Render Book Pages

--- Kindle/BookPagerController.swift
+++ Kindle/BookPagerController.swift
@@ -10,38 +10,44 @@ import UIKit
 
 class BookPagerController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    var book: Book?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .white
         
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         
-        navigationItem.title = "Book"
+        navigationItem.title = self.book?.title
         
         let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
         layout?.scrollDirection = .horizontal
         layout?.minimumLineSpacing = 0
         
         collectionView?.isPagingEnabled = true
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(handleCloseBook))
+    }
+    
+    @objc func handleCloseBook() {
+        dismiss(animated: true, completion: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
+        return CGSize(width: view.frame.width, height: view.frame.height - 60 - 62)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return book?.pages.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        let pageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! PageCell
         
-//        if indexPath.item % 2 == 0 {
-//            cell.backgroundColor = .red
-//        } else {
-//            cell.backgroundColor = .blue
-//        }
-        return cell
+        let page = book?.pages[indexPath.item]
+        pageCell.textLabel.text = page?.text
+        
+        return pageCell
     }
 }
 
